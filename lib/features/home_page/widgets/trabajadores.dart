1d7da0c5070bbf62cef15f8bf0ed9a905bbc2b96
LuciaPuppo897 <alerome897@gmail.com>
2024-02-28 13:33:13 -0300

    add class trabajador

--- lib/features/home_page/widgets/trabajadores.dart
+++ lib/features/home_page/widgets/trabajadores.dart
@@ -1,51 +1,16 @@
 // Definición de la clase Trabajador
 class Trabajador {
+  final String id;
   final String nombre;
   final String oficio;
   final String barrio;
   final String apellido;
   Trabajador({
+    required this.id,
     required this.nombre,
     required this.apellido,
     required this.oficio,
     required this.barrio,
+
   });
 }
-
-// Lista de trabajadores de ejemplo
-List<Trabajador> trabajadores = [
-  Trabajador(
-      nombre: 'Juan',
-      oficio: 'Carpintero',
-      barrio: 'Centro',
-      apellido: 'perez'),
-  Trabajador(
-      nombre: 'Pedro',
-      oficio: 'Electricista',
-      barrio: 'Cordon',
-      apellido: 'lopez'),
-  Trabajador(
-      nombre: 'María',
-      oficio: 'Plomero',
-      barrio: 'Punta Carretas',
-      apellido: 'rodriguez'),
-  Trabajador(
-      nombre: 'Ana', oficio: 'Albañil', barrio: 'Malvin', apellido: 'perez'),
-  Trabajador(
-      nombre: 'Martin', oficio: 'Albañil', barrio: 'Cordon', apellido: 'perez'),
-  Trabajador(
-      nombre: 'Ana',
-      oficio: 'Jardinero',
-      barrio: 'Bella Italia',
-      apellido: 'perez'),
-  Trabajador(
-      nombre: 'Ana', oficio: 'Pintor', barrio: 'Cordon', apellido: 'perez'),
-  Trabajador(
-      nombre: 'Laura',
-      oficio: 'Pintor',
-      barrio: 'La blanqueada',
-      apellido: 'perez'),
-  Trabajador(
-      nombre: 'Ana', oficio: 'Albañil', barrio: 'Cordon', apellido: 'Ferrari'),
-  // Agrega más trabajadores según tus necesidades
-];
