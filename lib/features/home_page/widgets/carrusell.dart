c08a532ccd9e346cfb5a5b788bd5bc0e881ba089
Yetzabeth Hernandez <6785@hbtn.uy>
2024-02-28 14:07:07 -0300

    updating home screen1

--- lib/features/home_page/widgets/carrusell.dart
+++ lib/features/home_page/widgets/carrusell.dart
@@ -2,10 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 
 class ImageCarousel extends StatelessWidget {
-  final List<String> imageAssetPaths;
+  final List<String> imageUrls;
 
-  const ImageCarousel({Key? key, required this.imageAssetPaths})
-      : super(key: key);
+  const ImageCarousel({Key? key, required this.imageUrls}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -23,23 +22,35 @@ class ImageCarousel extends StatelessWidget {
         enlargeCenterPage: true,
         onPageChanged: (index, reason) {},
       ),
-      items: imageAssetPaths.map((assetPath) {
+      items: imageUrls.map((imageUrl) {
         return Builder(
           builder: (BuildContext context) {
             return Container(
               width: MediaQuery.of(context).size.width,
               margin: const EdgeInsets.symmetric(horizontal: 5.0),
-              decoration: const BoxDecoration(
-                color: Colors.grey,
-              ),
-              child: Image.asset(
-                assetPath,
-                fit: BoxFit.cover,
-              ),
+              child: _buildImage(imageUrl),
             );
           },
         );
       }).toList(),
+    );
+  }
+
+  Widget _buildImage(String imageUrl) {
+    return Image.network(
+      imageUrl,
+      fit: BoxFit.cover,
+      loadingBuilder: (BuildContext context, Widget child, ImageChunkEvent? loadingProgress) {
+        if (loadingProgress == null) {
+          return child;
+        } else {
+          return Container(
+            color: Colors.transparent, // Color transparente para evitar que se muestre el contenedor gris
+            width: double.infinity,
+            height: 120,
+          );
+        }
+      },
     );
   }
 }
